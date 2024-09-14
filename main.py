0296b83d969f59cbdc6dd3374554cc7ef385a1d5
RoniFridman1 <ronifridman0@gmail.com>
2024-09-14 19:21:27 +0300

    params

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from experiment import run_experiment
 
 if __name__ == "__main__":
     DATA_DIR = "chest_xray"
-    MODELS = ["vgg16"]
+    MODELS = ["resnet18"]
     # MODELS = ["resnet18","vgg16"]
     SAMPLING_METHODS = ["uncertainty", "entropy",'random']
     ACTIVE_LEARNING_ITERATIONS = 10
-    MODEL_TRAINING_EPOCHS = 4
-    SAMPLES_PER_ITERATION = 30
-    TOTAL_TRAINING_SAMPLES = 500
+    MODEL_TRAINING_EPOCHS = 3
+    SAMPLES_PER_ITERATION = 10
+    TOTAL_TRAINING_SAMPLES = 250
     TOTAL_TEST_SAMPLES = 250
     BATCH_SIZE=25
     run_experiment(DATA_DIR, MODELS, SAMPLING_METHODS, ACTIVE_LEARNING_ITERATIONS, MODEL_TRAINING_EPOCHS,
