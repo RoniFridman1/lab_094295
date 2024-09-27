48b61216a158fe9d3b6fdbc3928c33697bbfe633
nimrod-solomon <s.nimrod@campus.technion.ac.il>
2024-09-27 17:50:48 +0300

    returned data_loader.py to as it was before the checkout to branch

--- data_loader.py
+++ data_loader.py
@@ -3,24 +3,6 @@ import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import numpy as np
-import PIL.Image
-
-
-class AugmentedImageFolder(datasets.ImageFolder):
-    def __init__(self, root, transform=None, num_augmentations=50):
-        super().__init__(root, transform)
-        self.augmented_samples = []
-        for img_path, label in self.samples:
-            img = PIL.Image.open(img_path)
-            for _ in range(num_augmentations):
-                augmented_img = self.transform(img)
-                self.augmented_samples.append((augmented_img, label))
-
-    def __getitem__(self, index):
-        return self.augmented_samples[index]
-
-    def __len__(self):
-        return len(self.augmented_samples)
 
 
 def load_data(data_dir: str, total_train_samples, batch_size, labeled_unlabeled_split=(0.25,0.75),
@@ -40,13 +22,11 @@ def load_data(data_dir: str, total_train_samples, batch_size, labeled_unlabeled_
         and test sets.
     """
     torch.manual_seed(seed)
-    # Resize images to 312x312 pixels -> Convert to PyTorch tensors. -> Normalizes to a standard range
+    # Resize images to 224x224 pixels -> Convert to PyTorch tensors. -> Normalizes to a standard range
     transform = transforms.Compose([
-        transforms.Resize((312, 312)),
-        transforms.RandomRotation(degrees=(-15, 15)),  # Randomly rotate images by -15 to 15 degrees
-        transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1)),  # Randomly translate and scale
+        transforms.Resize((312,312)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=0.5, std=0.5)
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
     # Set the paths for the data directories
@@ -55,11 +35,7 @@ def load_data(data_dir: str, total_train_samples, batch_size, labeled_unlabeled_
     test_dir = os.path.join(data_dir, 'test')
 
     # randomly select some of the samples in each dataset, and split the test set to labeled and unlabeled sets
-    # train_dataset = datasets.ImageFolder(train_dir, transform=transform)
-    train_dataset = AugmentedImageFolder(train_dir, transform=transform)
-    print(train_dataset[0])
-    print(train_dataset[50])
-    print(train_dataset[100])
+    train_dataset = datasets.ImageFolder(train_dir, transform=transform)
     val_dataset = datasets.ImageFolder(val_dir, transform=transform)
     test_dataset = datasets.ImageFolder(test_dir, transform=transform)
 
